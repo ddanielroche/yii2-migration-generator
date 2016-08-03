d078b95b63ec018e642cd0458904edca34d8502c
Dany Daniel Roche <dany.daniel@versat.azcuba.cu>
2016-08-03 09:41:49 -0400

    Añadiendo Bootstrap.php

---
+++ Bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ddanielroche\migration;
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     *
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        if ($app->hasModule('gii')) {
+            if (!isset($app->getModule('gii')->generators['migration'])) {
+                $app->getModule('gii')->generators['migration'] = 'ddanielroche\migration\Generator';
+            }
+        }
+    }
+}
