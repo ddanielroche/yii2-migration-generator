53232cba59343783cbd7b6a2c70544c8a1220d16
Dany Daniel Roche <dany.daniel@versat.azcuba.cu>
2016-12-12 17:57:05 -0500

    Instalando dependencias

---
+++ ci/docker_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && [[ ! -e /.dockerinit ]] && exit 0
+
+set -xe
+
+# Install git (the php image doesn't have it) which is required by composer
+apt-get update -yqq
+apt-get install php-zip git -yqq
+
+cd /opt/
+git clone https://git.versat.azcuba.cu/backend/composer.git
+composer/install.sh
